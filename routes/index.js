460e1bb5ba4b1a5e06276f8f00f66a5f2208a505
Raja Luqman Sarwar <luqmanrajput012@gmail.com>
2024-05-23 02:02:35 -0700

    feat(views): add signup, login , dashboard view

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 var express = require("express");
 var router = express.Router();
+const passport = require("passport");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index");
 });
 
+router.get("/dashboard", function (req, res, next) {
+  res.render("dashboard");
+});
+
 // signup page
 router.get("/signup", function (req, res, next) {
   res.render("signup");
+});
+
+// login page display
+router.get("/loginpage", function (req, res) {
+  res.render("login");
+});
+
+// login logic
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/loginpage",
+    failureFlash: true,
+  })
+);
+
+// logout logic
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
